d3011560de38440dc0a71beaecce8af722ee2a9a
pablo <pablo.rosales14@gmail.com>
2018-09-26 20:57:27 -0700

    ALmost there

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 var imgElement1 = document.getElementById('slot1');
 var imgElement2 = document.getElementById('slot2');
 var imgElement3 = document.getElementById('slot3');
+var ctx = document.getElementById('myChart');
+
 var maxClicksAllowed = 25;
 
 
@@ -105,6 +107,83 @@ function handleClick(){
   }
   shuffle();
 }
+
+//chart.js library start
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Bag', 'Banana', 'Bathroom', 'Boots', 'Breakfast', 'Bubblegum', 'Chair', 'Cthulhu', 'Dog-Duck', 'Dragon', 'Pen', 'Pet Sweep', 'Scissors', 'Shark', 'Sweep', 'Tauntaun', 'Unicorn', 'Usb', 'Water Can', 'Wine Glass'],
+    datasets: [{
+      label: [Product.allProducts[0].votes, Product.allProducts[1].votes, Product.allProducts[2].votes, Product.allProducts[3].votes, Product.allProducts[4].votes ,Product.allProducts[5].votes, Product.allProducts[6].votes, Product.allProducts[7].votes, Product.allProducts[8].votes, Product.allProducts[9].votes ,Product.allProducts[10].votes, Product.allProducts[11].votes, Product.allProducts[12].votes, Product.allProducts[13].votes, Product.allProducts[14].votes ,Product.allProducts[15].votes, Product.allProducts[16].votes, Product.allProducts[17].votes, Product.allProducts[18].votes, Product.allProducts[19].votes],
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)' ,
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    title:{
+      display: true,
+      text: 'Results of survey'
+    },
+    scales: {
+      gridLines:[{
+        tickMArkLength: 50
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          stepSize: 1,
+        },
+        weight: -20
+      }]
+    }
+  }
+});
+//chart.js library end
 
 setupEventListeners();
 initializeDatabase();
